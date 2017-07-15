2f82b5203bdbe975b1cdfef5c89aae695c57f5e1
͡° ͜ʖ ͡° <artgreg@outlook.fr>
2017-07-15 21:47:28 +0200

    date to json

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function placeCircles() {
     }
   }
   //console.log(htmlOutput);
-  htmlOutput += '<div id="date" style="position: absolute; top: 300px; left: '+((w / 2) - circleWidth)+'px;"><h1>'+d.getDate()+'/'+(d.getMonth() + 1)+'/'+d.getFullYear()+'</h1></div>';
+  htmlOutput += '<div id="date" style="position: absolute; top: 300px; left: '+((w / 2) - circleWidth)+'px;"><h1>'+new Date().toJSON().substr(0, 10)+'</h1></div>';
   document.querySelector('#main').innerHTML = htmlOutput;
 
 }
